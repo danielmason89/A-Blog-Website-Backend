06296bd3d400b285ad4cff25659d838b1a5303db
Mbonu Basil <mbonubasil@gmail.com>
2025-03-21 18:08:27 -0400

    Setup user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,4 +14,4 @@ import { UsersModule } from './users/users.module';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ default: 'user' })
+  role: string;
+
+  @Prop({ default: 'active' })
+  status: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+
+  // posts: Post[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
